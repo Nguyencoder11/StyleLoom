ef6e29278b70800d61b9ec67f4a215de7f8216fe
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2024-05-14 00:51:29 +0700

    :memo: update java springboot backend

---
+++ styleloom-server/src/main/java/com/mycompany/styleloom/server/StyleloomServer.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.styleloom.server;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class StyleloomServer {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
